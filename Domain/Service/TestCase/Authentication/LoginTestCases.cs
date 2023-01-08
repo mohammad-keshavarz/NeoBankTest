85e7ef8acf44bde96ffea27dcbc391570df31cdc
Vahid Saberi <saberi1365@gmail.com>
2023-01-08 18:08:12 +0330

    errors ok

--- Domain/Service/TestCase/Authentication/LoginTestCases.cs
+++ Domain/Service/TestCase/Authentication/LoginTestCases.cs
@@ -1,92 +1,97 @@
-﻿using Azure;
-using Domain.Models;
-using Domain.Models.Authenticate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Models;
 
 namespace Domain.Service.TestCase.Authentication;
 
 public static class LoginTestCases
 {
-	/*		 public async TestCaseDTO<LoginRequestDTO, LoginResponseDTO> loginTestCases() {
+    /*		 public async TestCaseDTO<LoginRequestDTO, LoginResponseDTO> loginTestCases() {
 TestCaseDTO<LoginRequestDTO, LoginResponseDTO>[] loginTestCaseList;
 loginTestCaseList[0]= new TestCaseDTO<>
 }*/
 
-	public static List<RequestDTO<dynamic, dynamic>> LoginTestList { get; } = new()
-	{
-		new RequestDTO<dynamic, dynamic> {
-			PBIId = 14007,
-			ScenarioId = 0,
-			TestCaseId = 14116,
-			Uri = "authentication/login",
+    public static List<RequestDTO<dynamic, dynamic>> LoginTestList { get; } = new()
+    {
+        new RequestDTO<dynamic, dynamic>
+        {
+            PBIId = 14007,
+            ScenarioId = 0,
+            TestCaseId = 14116,
+            Uri = "authentication/login",
 
-			ExpectedStatus = 200,
-			Body = new {
-				nationalCode= "0013013203",
-				password = "Elh@m121169"
-			},
-			ExpectedResult = new
-			{
+            ExpectedStatus = 200,
+            Body = new
+            {
+                nationalCode = "0013013203",
+                password = "Elh@m121169"
+            },
+            ExpectedResult = new
+            {
                 data = "null",
-				message= "null",
-				action= "Panel",
-				succeeded= true,
-				errors= "null"
-			},
-			ServiceType = RequestType.Post
-		},
+                message = "null",
+                action = "Panel",
+                succeeded = true,
+                errors = "null"
+            },
+            ServiceType = RequestType.Post
+        },
 
-		new RequestDTO<dynamic, dynamic> {
-			TestCaseId = 14119,
-			ScenarioId = 0,
-			PBIId = 14007,
-			Uri = "authentication/login",
+        new RequestDTO<dynamic, dynamic>
+        {
+            TestCaseId = 14119,
+            ScenarioId = 0,
+            PBIId = 14007,
+            Uri = "authentication/login",
 
-			ExpectedStatus = 200,
-			Body = new {
-				nationalCode= "0013013203",
-				password = "Elh@m1211693"
-			},
-			ExpectedResult = new
-			{
-				data = "null",
-				message= "null",
-				action= "Panel",
-				succeeded= true,
-				errors= "null"
-			},
-			ServiceType = RequestType.Post
-		},
+            ExpectedStatus = 200,
+            Body = new
+            {
+                nationalCode = "0013013203",
+                password = "Elh@m1211693"
+            },
+            ExpectedResult = new
+            {
+                data = "null",
+                message = "null",
+                action = "Panel",
+                succeeded = true,
+                errors = "null"
+            },
+            ServiceType = RequestType.Post
+        },
 
-		new RequestDTO<dynamic, dynamic> {
-			TestCaseId = 14021,
-			ScenarioId = 14117,
-			PBIId = 14007,
-			Uri = "authentication/login",
+        new RequestDTO<dynamic, dynamic>
+        {
+            TestCaseId = 1,
+            ScenarioId = 1111,
+            PBIId = 14007,
+            Uri = "authentication/login",
 
-			ExpectedStatus = 200,
-			Body = new {
-				nationalCode= "001301320",
-				password = "Elh@m1211693"
-			},
-			ExpectedResult = new
-			{
-				data = "null",
-				message= "null",
-				action= "Panel",
-				succeeded= true,
-				errors= "null"
-			},
-			ServiceType = RequestType.Post
-		},
-
-
-
-	};
+            ExpectedStatus = 400,
+            Body = new
+            {
+                nationalCode = "1234567890",
+                password = "Elh@m1211693"
+            },
+            ExpectedResult = new
+            {
+                data = "null",
+                message = "null",
+                action = "null",
+                succeeded = false,
+                errors = new[]
+                {
+                    new {
+                        message = "مقدار وارد شده برای فیلد کد ملی معتبر نمی باشد",
+                        code = "null",
+                        reference = "NationalCode",
+                        info = "null",
+                        value = "null"
+                    },
+                },
+            },
+            ServiceType = RequestType.Post
+        }
+    };
 
 
 
